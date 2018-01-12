46bf1381171c5afe326d27f05d9b6d907df174fc
ronok <roonok@users.noreply.github.com>
2018-01-13 03:22:29 +0600

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  
-  HackerRank Url :: https://www.hackerrank.com/challenges/java-inheritance-1/problem
+  HackerRank Url :: https://www.hackerrank.com/challenges/java-inheritance-2/problem
  
  
  */
