411def8957c088a972eae3a38b8d8885b7c91fa7
ronok <roonok@users.noreply.github.com>
2018-01-13 03:21:43 +0600

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -2,6 +2,10 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ 
+  HackerRank Url :: https://www.hackerrank.com/challenges/java-inheritance-1/problem
+ 
+ 
  */
 package testappv1;
 
