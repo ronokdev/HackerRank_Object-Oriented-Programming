07ed564a2cb6a77e92d79d98609801a04c5a4f8a
ronok <roonok@users.noreply.github.com>
2018-01-13 03:23:21 +0600

    Update TestAppV1.java

--- TestAppV1.java
+++ TestAppV1.java
@@ -2,6 +2,9 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ 
+ HackerRank URL :: https://www.hackerrank.com/challenges/java-inheritance-1
+ 
  */
 package testappv1;
 
