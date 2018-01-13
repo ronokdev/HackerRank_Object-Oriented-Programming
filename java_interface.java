b0b158088a6198b9c5a24e3ac54b2671de48aa6e
ronok <roonok@users.noreply.github.com>
2018-01-14 01:18:07 +0600

    Add files via upload

---
+++ java_interface.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testappv1;
+
+/**
+ *
+ * @author farhan
+ */
+public class java_interface  {
+    public static void main (String args[]){
+        MyCalculator myCal = new MyCalculator();
+        System.out.println("I implemented:"+ myCal.getClass().getInterfaces()[0]);
+        int Result = myCal.divisor_sum(20);
+        
+        System.out.println(Result);
+    }
+    
+}
+
+
+interface AdvancedArithmetic{
+  int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic{
+    @Override
+    
+    public int divisor_sum(int n){
+       
+        int counter = n,sum=0;
+        
+        while(counter > 0){
+            //System.out.println(counter);
+            if(n%counter == 0){
+                  sum = sum + counter;  
+            }
+            counter--;
+        }
+        return sum;
+    }
+}
